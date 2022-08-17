b1d074a33e2f4be4771a3749de2266f3543ba2dc
Klincher <camelotvm48@gmail.com>
2022-08-17 10:39:29 +0300

    save orders in db and added bootstrap

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -13,7 +13,7 @@ class OrderController extends Controller
     public function index()
     {
         $rooms = ['No rooms', '1 room', '2 rooms', '3 rooms', '4 rooms', '5 rooms', '6 rooms', '7 rooms', '8 rooms', '9 rooms', '10 rooms'];
-        $bathrooms = ['0'=> 'No toilets', '0.5' => 'Toilet', '1' => 'Bathroom', '1.5' => 'Combined bathroom'];
+        $bathrooms = ['0' => 'No toilets', '0.5' => 'Toilet', '1' => 'Bathroom', '1.5' => 'Combined bathroom'];
         $kitchens = ['No kitchens', '1 kitchen', '2 kitchens', '3 kitchens', '4 kitchens', '5 kitchens'];
 
         if ($id = session('order_id')) {
@@ -46,29 +46,25 @@ class OrderController extends Controller
             ]
         );
 
-        // if ($request->session()->has('order_id')) {
-        //     $order = Order::find(intval(session()->get('order_id')));
-        // } else {
-        //     $order = new Order;
-        //     $order->client_id = $client->id;
-        // // }
+        $order = new Order;
+        $order->client_id = $client->id;
 
-        // $order->status = 'created';
-        // $order->sum = $sum;
-        // $order->address = $request->address;
-        // $order->area = $request->area;
-        // $order->rooms = $request->rooms ?? 0;
-        // $order->bathrooms = $request->bathrooms ?? 0;
-        // $order->kitchens = $request->kitchens ?? 0;
-        // $order->fridges = $request->fridges ?? 0;
-        // $order->wardrobes = $request->wardrobes ?? 0;
-        // $order->animals = $request->animals ?? 0;
-        // $order->adults = $request->adults ?? 0;
-        // $order->children = $request->children ?? 0;
-        // $order->save();
+        $order->status = 'created';
+        $order->sum = $sum;
+        $order->address = $request->address;
+        $order->area = $request->area;
+        $order->rooms = $request->rooms ?? 0;
+        $order->bathrooms = $request->bathrooms ?? 0;
+        $order->kitchens = $request->kitchens ?? 0;
+        $order->fridges = $request->fridges ?? 0;
+        $order->wardrobes = $request->wardrobes ?? 0;
+        $order->animals = $request->animals ?? 0;
+        $order->adults = $request->adults ?? 0;
+        $order->children = $request->children ?? 0;
+        $order->save();
 
-        // session(['order_id' => $order->id]);
+        session(['order_id' => $order->id]);
 
-        // return redirect()->route('result');
+        return redirect()->route('result');
     }
 }
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,26 +1,55 @@
 <template>
-    <div>
-        <v-form>
-            <v-container class="col-3">
-                <v-text-field v-model="formData.phone" label="Phone" outlined></v-text-field>
-                <v-text-field v-model="formData.first_name" label="First Name" outlined></v-text-field>
-                <v-text-field v-model="formData.last_name" label="Last Name" outlined></v-text-field>
-                <v-text-field v-model="formData.email" label="Email" outlined></v-text-field>
-                <v-btn text small color="primary" @click="saveClient">Vuetify hueta</v-btn>
-            </v-container>
-        </v-form>
-    </div>
+    <v-form>
+        <v-container>
+            <v-row>
+                <v-col sm="6">
+                    <v-text-field v-model="formData.phone" label="Phone" outlined></v-text-field>
+                    <v-text-field v-model="formData.first_name" label="First Name" outlined></v-text-field>
+                    <v-text-field v-model="formData.last_name" label="Last Name" outlined></v-text-field>
+                    <v-text-field v-model="formData.email" label="Email" outlined></v-text-field>
+                </v-col>
+                <v-col sm="6">
+                    <v-text-field v-model="formData.address" label="Address" outlined></v-text-field>
+                    <v-text-field v-model="formData.area" label="Area" outlined></v-text-field>
+                    <v-select v-model="formData.rooms" :items="rooms" label="Rooms" outlined></v-select>
+                    <v-select v-model="formData.bathrooms" :items="bathrooms" label="Bathrooms" outlined></v-select>
+                    <v-select v-model="formData.kitchens" :items="kitchens" label="Kitchens" outlined></v-select>
+                    <v-text-field v-model="formData.fridges" label="Fridges" outlined></v-text-field>
+                    <v-text-field v-model="formData.wardrobes" label="Wardrobes" outlined></v-text-field>
+                    <v-text-field v-model="formData.animals" label="Animals" outlined></v-text-field>
+                    <v-text-field v-model="formData.adults" label="Adults" outlined></v-text-field>
+                    <v-text-field v-model="formData.children" label="Children" outlined></v-text-field>
+                    <div class="d-flex justify-content-center">
+                        <v-btn depressed color="primary" @click="saveClient">Confirm</v-btn>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
 
 <script>
 export default {
     data() {
         return {
+            rooms: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+            bathrooms: ['0,5', '1', '1,5'],
+            kitchens: ['1', '2', '3'],
             formData: {
                 phone: '',
                 first_name: '',
                 last_name: '',
                 email: '',
+                address: '',
+                area: '',
+                rooms: '0',
+                bathrooms: '0',
+                kitchens: '0',
+                fridges: '',
+                wardrobes: '',
+                animals: '',
+                adults: '',
+                children: '',
             },
         }
     },
@@ -30,7 +59,6 @@ export default {
                 .post('/api/clients', this.formData)
                 .then(({ data }) => {
                     console.log('data :>> ', data);
-                    // this.clients = data;
                 })
                 .catch(({ message }) => {
                     console.log('error :>> ', message);
